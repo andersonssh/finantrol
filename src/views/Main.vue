c3302440b57efeda49ecd7707b0804d3c8fb583d
Anderson Araujo <andersondev.ssh@gmail.com>
2023-10-29 23:00:50 -0300

    Add basic structure of main app

---
+++ src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="h-full pt-16 pb-24 overflow-y-auto bg-yellow-500">
+        
+
+    </div>
+    <div class="absolute bottom-0 w-full bg-red-500 text-center">
+        x
+    </div>
+
+    
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Main',
+    data() {
+        return {
+            msg: 'Hello Vite + Vue'
+        }
+    }
+}
+</script>
