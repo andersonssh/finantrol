bbbe07f70a0859d548dbf0003ce691c34dc59d16
Anderson Araujo <andersondev.ssh@gmail.com>
2023-11-06 08:26:14 -0300

    Add main total count

--- src/register.ts
+++ src/register.ts
@@ -16,3 +16,24 @@ export function getRegisterValue(register: Register) {
     return register.value;
 }
 
+export function getTotal(registers: Array<Register>){
+    let sumEntries = getEntries(registers).reduce(
+        (acc, reg) => acc + getRegisterValue(reg), 0)
+    let sumExits = getExits(registers).reduce(
+        (acc, reg) => acc + getRegisterValue(reg), 0)
+    return sumEntries - sumExits
+}
+
+
+export function getEntries(registers: Array<Register>){
+    return registers.filter((reg) => !isExpense(reg))
+}
+
+export function getExits(registers: Array<Register>){
+    return registers.filter((reg) => isExpense(reg))
+}
+
+
+export function isExpense(register: Register){
+    return register.category != "entradas"
+}
