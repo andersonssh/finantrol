1b6c4171f5cfbfe623c5405e8680acc909516fb7
Anderson Araujo <andersondev.ssh@gmail.com>
2023-11-06 22:23:17 -0300

    Change font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 export default {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ['"Rubik"', 'sans-serif']
+        }
     },
 };
